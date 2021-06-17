9871eeaf6346846aead19c832ac3ed1e2c019782
jehfyahala <lase1705@gmail.com>
2021-06-17 17:53:26 -0600

    Creacion de proyecto

---
+++ RegistroQuejas/RegistroQuejas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RegistroQuejas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
