28c778b2ddcbcfd568851c0668683051e802cfb6
jehfyahala <lase1705@gmail.com>
2021-06-18 03:52:44 -0600

    actualizacion final

---
+++ RegistroQuejas/RegistroQuejas/RegistroQuejas/Data/Migrations/20210618094520_UpdateComplainsName.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroQuejas.Data.Migrations
+{
+    public partial class UpdateComplainsName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaQueja",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaPrueba",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaNombre",
+                table: "Complains",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaHechos",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaFecha",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaEmail",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaDependencia",
+                table: "Complains",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaCopia",
+                table: "Complains",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaCargo",
+                table: "Complains",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaQueja",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaPrueba",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaNombre",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaHechos",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaFecha",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaEmail",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaDependencia",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaCopia",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuejaCargo",
+                table: "Complains",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
--- RegistroQuejas/RegistroQuejas/RegistroQuejas/Models/Complain.cs
+++ RegistroQuejas/RegistroQuejas/RegistroQuejas/Models/Complain.cs
@@ -10,35 +10,48 @@ namespace RegistroQuejas.Models
     {
         [Key]
         public int QuejaId { get; set; }
+        [Required(ErrorMessage = "La fecha es obligatorio")]
         [Display(Name = "Fecha:")]
 
         public string QuejaFecha { get; set; }
         [Required(ErrorMessage = "El nombre es obligatorio")]
         [Display(Name = "Nombre:")]
+        [StringLength(50,ErrorMessage ="No debe tener mas de 50 caracteres.")]
+        [MinLength(3, ErrorMessage ="Debe tener mas de 3 caracteres.")]
         public string QuejaNombre { get; set; }
         [Display(Name = "Identidad:")]
 
         public string QuejaDni { get; set; }
         [Display(Name = "C.C.:")]
+        [StringLength(50, ErrorMessage = "No debe tener mas de 50 caracteres.")]
+        [MinLength(3, ErrorMessage = "Debe tener mas de 3 caracteres.")]
 
         public string QuejaCopia { get; set; }
         [Display(Name = "Número:")]
-
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public int QuejaNum { get; set; }
         [Display(Name = "Dependencia:")]
+        [StringLength(50, ErrorMessage = "No debe tener mas de 50 caracteres.")]
+        [MinLength(3, ErrorMessage = "Debe tener mas de 3 caracteres.")]
 
         public string QuejaDependencia { get; set; }
         [Display(Name = "Cargo:")]
+        [StringLength(50, ErrorMessage = "No debe tener mas de 50 caracteres.")]
+        [MinLength(3, ErrorMessage = "Debe tener mas de 3 caracteres.")]
 
         public string QuejaCargo { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         [Display(Name = "Correo Electrónico:")]
-
+        
+        //[DataType(DataType.EmailAddress, ErrorMessage = "Por favor, ingrese un email válido")]
+        [EmailAddress(ErrorMessage = "Por favor, ingrese un email válido")]
         public string QuejaEmail { get; set; }
         [Display(Name = "QUEJAS")]
-
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string QuejaQueja { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string QuejaHechos { get; set; }
-
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string QuejaPrueba { get; set; }
     }
 }
